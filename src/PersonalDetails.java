dcb8ea202470cb0353cd80504e480c1c684ddd50
milan <milan.kvd1991@gmail.com>
2019-07-29 14:21:49 +0100

    Personaldetails added

---
+++ src/PersonalDetails.java
@@ -0,0 +1,18 @@
+public class PersonalDetails {
+    public static void main (String args[])
+    {
+        String name="John Root";
+        int age=25;
+        String address="A/1 kingsly road, harrow, HA32PR";
+        String mobile="+449898788505";
+        float salary=75000;
+
+
+        System.out.println("Name: " +name);
+        System.out.println("age: "+age);
+        System.out.println("address: "+address);
+        System.out.println("mobile: "+mobile);
+        System.out.println("salary: "+salary);
+
+    }
+}
